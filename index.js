cdd391af28fb0f8b0817c7a182522b324557cf39
Oleksandr Kononov <oleksandr.kononov@errigal.com>
2020-03-23 19:43:01 +0000

    added API Documentation

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
 import bodyParser from 'body-parser';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 
 dotenv.config();
 
 const app = express();
 
 const port = process.env.PORT;
+
+const swaggerDocument = YAML.load('movie-api-yaml/swagger.yaml');
 
 //configure body-parser
 app.use(bodyParser.json());
@@ -16,6 +20,7 @@ app.use(bodyParser.urlencoded());
 app.use(express.static('public'));
 
 app.use('/api/movies', moviesRouter);
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
